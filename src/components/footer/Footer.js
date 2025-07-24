4291aabcd1ee15180623d39c64e0898cda8a6993
Mohamed ahmed fathelbab <fathelbabmohamed94@gmail.com>
2025-07-24 21:35:34 +0300

    Fix content

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,7 +12,7 @@ export default function Footer() {
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           {emoji("Made with ❤️ by Fath Elbab")}
         </p>
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+        {/* <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
           <a
             href="https://github.com/fathelbab"
@@ -21,7 +21,7 @@ export default function Footer() {
           >
             Fathelbab
           </a>
-        </p>
+        </p> */}
       </div>
     </Fade>
   );
